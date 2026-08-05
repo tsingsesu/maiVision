126dfd4cadd65b11072d3df226205621533230e1
tsingsesu <tsingsesu@163.com>
2026-08-06 01:44:08 +0800

    the simplest face detection

--- maivideo.py
+++ maivideo.py
@@ -1,13 +1,21 @@
 import cv2
 
 capture = cv2.VideoCapture(0)
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades +\
+'haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = capture.read()
-    key = cv2.waitKey(1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    #灰度用来检测
-    cv2.rectangle(frame,(20,20),(100,100),(0,255,0),2) #彩色用来画框
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #灰度用来检测
+    faces = face_detector.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
     cv2.imshow('video', frame)
+
+    key = cv2.waitKey(1)
     if key == 113:  # 按下 'q' 键退出循环
         break
 capture.release()
