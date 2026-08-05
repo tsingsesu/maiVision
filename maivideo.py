5629cb481f281351ad6fc424dc9eed7500becb93
tsingsesu <tsingsesu@163.com>
2026-08-05 23:10:28 +0800

    first phrase

--- maivideo.py
+++ maivideo.py
@@ -5,10 +5,9 @@ capture = cv2.VideoCapture(0)
 while True:
     ret, frame = capture.read()
     key = cv2.waitKey(1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)  # 让灰度图获得三通道BGR
-    cv2.rectangle(gray_bgr,(20,20),(100,100),(0,255,0),2)
-    cv2.imshow('video', gray_bgr)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    #灰度用来检测
+    cv2.rectangle(frame,(20,20),(100,100),(0,255,0),2) #彩色用来画框
+    cv2.imshow('video', frame)
     if key == 113:  # 按下 'q' 键退出循环
         break
 capture.release()
