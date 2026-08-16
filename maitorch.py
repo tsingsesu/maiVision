63b6aef328e6f3e6a9a33e2390290a372c276536
tsingsesu <tsingsesu@163.com>
2026-08-16 12:38:47 +0800

    0816

--- maitorch.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.__version__)           # 输出 2.11.0+cu124 这种，+cu124代表cuda12.4版本
-print(torch.cuda.is_available())   # 返回True才算GPU可用；False=GPU没跑起来
-print(torch.cuda.get_device_name(0)) # 输出 RTX 5060
